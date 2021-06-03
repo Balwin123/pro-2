6b5101263722e34ee2b46403d8468bec35b3db4d
Balwin123 <balwinmk@gmail.com>
2021-06-03 13:06:19 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// function
+// creating a function
+
+function square(num1, num2){
+    var result = num1 - num2;
+    return result;
+}
+var x = square()
+function greeting(){
+    var name = prompt("what is your name");
+    var result = "Hello " +name;
+    console.log(result);
+}
+greeting();
+
+var num = 0;
+while(num < 100){
+    num += 1;
+    console.log(num);
+}
+for(let num = 0; num <= 100; num++){
+    console.log(num);
+}
